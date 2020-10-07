033f5f56b4ddc25072071e20ca8aef995c3fdb19
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-07 13:05:41 +0200

    insert v-spacer and sihn out button with an icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,28 @@
 <template>
-<nav >
-    <v-toolbar flat app class="grey" src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-        <v-toolbar-title>
-            <span class="font-weight-light">Todo</span>
-            <span>Projects</span>
-        </v-toolbar-title>
-
+  <nav>
+    <v-toolbar
+      flat
+      app
+      class="grey text-capital grey--text"
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      height="120"
+    >
+      <v-toolbar-title>
+        <span class="font-weight-light">Todo</span>
+        <span>Projects</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign out</span>
+        <v-icon right >mdi mdi-exit-to-app </v-icon>
+      </v-btn>
     </v-toolbar>
-</nav>
-
+  </nav>
 </template>
 
 <script>
 // eslint-disable-next-line no-irregular-whitespace
-export default {
-name: "Navbar"
-}
+export default {
+  name: "Navbar",
+};
 </script>
-
