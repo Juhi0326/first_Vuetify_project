a940f85a461e04ee73447c79c9b1d7c4c9c83b13
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-07 11:05:29 +0200

    game with buttons and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,19 @@
   <div class="home">
     <h1>This is the Home page</h1>
     <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed dark color="pink">Click me again</v-btn>
+    <v-btn text color="primary">Click me again</v-btn>
+    <v-btn text dark small color="pink">
+       Click me again
+      <v-icon right>
+        mdi-pencil
+      </v-icon>
+    </v-btn>
+        <v-btn fab dark small color="pink">
+      <v-icon>
+        mdi mdi-bug
+      </v-icon>
+    </v-btn>
   </div>
 </template>
 
@@ -12,10 +25,9 @@ export default {
 };
 </script>
 
-    <!-- csak akkor műkodik a saját style, ha nincs mögötte a scope kifejezés! -->
+<!-- csak akkor műkodik a saját style, ha nincs mögötte a scope kifejezés! -->
 <style>
 .uppercase {
- text-transform: uppercase;
+  text-transform: uppercase;
 }
-
 </style>
