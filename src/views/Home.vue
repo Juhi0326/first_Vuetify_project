d42d0c01a94260587c04831bc6082bff3b7a3781
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-07 10:32:40 +0200

    new formatted button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <h1>This is the Home page</h1>
-    
+    <v-btn class="pink white--text">Click me</v-btn>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Home',
-  components: {
-
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
+
+    <!-- csak akkor műkodik a saját style, ha nincs mögötte a scope kifejezés! -->
+<style>
+.uppercase {
+ text-transform: uppercase;
+}
+
+</style>
