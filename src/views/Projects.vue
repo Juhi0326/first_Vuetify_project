bd95c03216dfab30cb720b67cf5f1b7cd12575a0
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-08 16:47:12 +0200

    set a static expansion panel

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,19 @@
   <div class="Projects">
     <h1 class="subheading grey--text">This is projects page</h1>
     <v-container class="my-5">
-      content
+      <template>
+        <v-expansion-panels>
+          <v-expansion-panel v-for="(item, i) in 1" :key="i">
+            <v-expansion-panel-header>
+              Item
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <div class="font-weight-bold">2020.10.17</div>
+              <div>info</div>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </template>
     </v-container>
   </div>
 </template>
