23396542541493916c6aad2449f952328b3a7646
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-08 11:59:41 +0200

    set dynamic calsses for project status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,20 @@
   <div class="Dashboard">
     <h1 class="subheading grey--text">This is the Dashboard page</h1>
     <v-container class="my-5">
-      <v-card class="pa-4">
-        <v-row>
+      <v-card class="pa-4" v-for="(project, index) in projects" :key="index">
+        <v-row :class="`pa-3 project ${project.status}`">
           <v-col cols="12" md="6">
             <div class="caption grey--text">
               Project title
             </div>
-            <div>Create a new website</div>
+            <div>{{ project.title}}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">
               Person
             </div>
             <div>
-              Juhász István
+              {{ project.person}}
             </div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
@@ -23,7 +23,7 @@
               Due by
             </div>
             <div>
-              2020.11.01
+              {{ project.due}}
             </div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
@@ -31,7 +31,7 @@
               Status
             </div>
             <div>
-              ongoing
+              {{ project.status}}
             </div>
           </v-col>
         </v-row>
@@ -43,6 +43,58 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "Juhász István",
+          due: "2020.11.01",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi reprehenderit accusantium itaque distinctio officia ratione error. Soluta, consequatur? Vitae at eum deleniti, iste veniam sapiente! Id hic nisi magni doloribus?",
+        },
+        {
+          title: "Code the homepage",
+          person: "Bigos András",
+          due: "2020.12.01",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi reprehenderit accusantium itaque distinctio officia ratione error. Soluta, consequatur? Vitae at eum deleniti, iste veniam sapiente! Id hic nisi magni doloribus?",
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Borzsák Anna",
+          due: "2020.10.01",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi reprehenderit accusantium itaque distinctio officia ratione error. Soluta, consequatur? Vitae at eum deleniti, iste veniam sapiente! Id hic nisi magni doloribus?",
+        },
+        {
+          title: "Create a community forum",
+          person: "Márkus Szabolcs",
+          due: "2020.10.01",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi reprehenderit accusantium itaque distinctio officia ratione error. Soluta, consequatur? Vitae at eum deleniti, iste veniam sapiente! Id hic nisi magni doloribus?",
+        },
+      ],
+    };
+  },
   components: {},
 };
 </script>
+
+<style>
+
+.project.complete {
+  border-left: 4px solid rgb(0, 255, 0)  ;
+}
+.project.ongoing {
+  border-left: 4px solid rgb(255, 192, 0)  ;
+}
+.project.overdue {
+  border-left: 4px solid rgb(255, 0, 0)  ;
+}
+
+</style>
