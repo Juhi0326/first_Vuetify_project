d024c3d051d993fb3f26f2d0188d4b95ef671ce7
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-08 10:43:32 +0200

    grid example

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,27 @@
   <div class="Dashboard">
     <h1 class="subheading grey--text">This is the Dashboard page</h1>
     <v-container class="my-5">
-      content
+      <v-row justify="center">
+        <v-col cols="12" md="3">
+          <v-btn outlined block text color="grey">click me</v-btn>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-btn outlined block text color="grey">click me again</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center" no-gutters>
+        <v-col cols="12" md="3">
+          
+            <v-btn outlined block text color="grey">click me</v-btn>
+     
+        </v-col>
+        <v-col cols="12" md="3">
+        
+            <v-btn outlined block text color="grey">click me again</v-btn>
+         
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
