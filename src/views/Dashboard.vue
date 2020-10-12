47fd8b09f2d90db330686aef654890c01c6e440e
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-12 16:52:33 +0200

    remove v-dialog from v-for list

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -47,7 +47,7 @@
             <div class="caption grey--text">
               Project title
             </div>
-            <div>{{ project.title }}</div>
+            <div>{{ project.title }} {{ project.id }}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">
@@ -73,37 +73,40 @@
             </div>
           </v-col>
           <v-col cols="6" sm="2" md="1">
-            <v-dialog v-model="dialog" width="600px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" dark v-bind="attrs" v-on="on" class="my-2">
-                  complete
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline"
-                    >Are you sure to set project status to complete?</span
-                  >
-                </v-card-title>
-                <v-card-text>
-                  After you set this project complete you will not to change
-                  this status!
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" text @click="dialog = false">
-                    Yes
-                  </v-btn>
-                  <v-btn color="green darken-1" text @click="dialog = false">
-                    No
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <v-btn color="primary" dark @click="dialog = true" class="my-2">
+              complete
+            </v-btn>
           </v-col>
         </v-row>
         <v-divider></v-divider>
       </v-card>
+      <v-dialog v-model="dialog" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark v-bind="attrs" v-on="on" class="my-2">
+            complete
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline"
+              >Are you sure to set project status to complete?</span
+            >
+          </v-card-title>
+          <v-card-text>
+            After you set this project complete you will not to change this
+            status!
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              Yes
+            </v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              No
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </div>
 </template>
@@ -152,6 +155,7 @@ export default {
       });
     });
   },
+  updated() {},
   methods: {
     sortBy(prop) {
       this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
