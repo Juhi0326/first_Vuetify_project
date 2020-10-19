7e07a7a2e0a30137ac682ed7cef48ea54ae96a86
István Juhász <istvan.juhasz@telcotrend.hu>
2020-10-19 10:24:05 +0200

    create store, and setuo main js for vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import store from './Store'
 
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate)
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
