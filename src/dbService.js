216eb4bc9af51343cb9d783c9704d0dc38cd83f3
István Juhász <istvan.juhasz@telcotrend.hu>
2020-11-26 09:34:16 +0100

    modify db method with a console log

--- src/dbService.js
+++ src/dbService.js
@@ -3,15 +3,27 @@ import db from "@/fb";
 //import * as firebase from "firebase/app";
 import "firebase/auth";
 
+const firestore = true;
+const mySql = false;
+
 export function setCompleteProject(project, id) {
-    try {
-        db.collection(project)
-        .doc(id)
-        .update({ status: "completed" })
-        return "kiírtam valamit"
-    } catch (error) {
-        return error
+    
+    if (firestore) {
+        //firestor method
+        try {
+            db.collection(project)
+            .doc(id)
+            .update({ status: "completed" })
+            console.log("kiírtam valamit");
+        } catch (error) {
+            console.log(error);
+        }
+    } else if (mySql) {
+        //MySql method
+        console.log("mySql method",mySql)
+        
     }
+ 
 }
 
 
